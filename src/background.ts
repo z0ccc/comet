7e2d4eefe36e939e7eee9bed2f325c0dd6f8ee7a
z0ccc <z0cccccccc@gmail.com>
2021-08-09 21:17:55 -0400

    Removed unneeded code and added comments

--- src/background.ts
+++ src/background.ts
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 // This file is ran as a background script
 import {
   getQueries,
@@ -9,19 +8,19 @@ import {
   DataType,
 } from './types';
 
-function setIcon(postArr: DataType[]) {
+// Changes icon color
+const setIcon = (postArr: DataType[]) => {
   const icon = postArr.length ? 'images/reddit_16.png' : 'images/grey_16.png';
   chrome.action.setIcon({
     path: {
       16: icon,
     },
   });
-}
+};
 
 chrome.tabs.onUpdated.addListener((tabId, change, tab) => {
   chrome.storage.sync.get('clickOnly', ({ clickOnly }) => {
     if (!clickOnly) {
-      console.log(getQueries(tab.url!));
       const queries: string[] = getQueries(tab.url!);
       getPostArr(queries).then((postArr) => {
         setIcon(postArr);
@@ -34,7 +33,6 @@ chrome.tabs.onActivated.addListener(() => {
   chrome.storage.sync.get('clickOnly', ({ clickOnly }) => {
     if (!clickOnly) {
       chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
-        console.log(getQueries(tabs[0].url!));
         const queries: string[] = getQueries(tabs[0].url!);
         getPostArr(queries).then((postArr) => {
           setIcon(postArr);
