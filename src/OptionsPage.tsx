87c72f656c2b65fbf6ac260b695c40e54b120d27
z0ccc <z0cccccccc@gmail.com>
2021-08-11 15:03:18 -0400

    Added options css and html

--- src/OptionsPage.tsx
+++ src/OptionsPage.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable arrow-body-style */
 /* eslint-disable no-unused-vars */
 import * as React from 'react';
@@ -6,7 +7,27 @@ import { useState, useEffect } from 'react';
 import './App.css';
 
 const OptionsPage = () => {
-  return <div className="App">jjj</div>;
+  return (
+    <div>
+      <div className="optionItemWrap">
+        <select className="selectBoxWrap" name="theme" id="theme">
+          <option value="default">Default</option>
+          <option value="dark">Dark</option>
+          <option value="light">Light</option>
+        </select>
+        <label htmlFor="theme">Extension theme</label>
+      </div>
+      <div className="optionItemWrap">
+        <input type="checkbox" id="clickOnly" name="clickOnly" />
+        <label htmlFor="clickOnly">Run browser popup only when icon is clicked</label>
+      </div>
+      <div className="optionItemWrap">
+        <input type="checkbox" id="ytDefault" name="ytDefault" />
+        <label htmlFor="ytDefault">Show YouTube comments as default</label>
+      </div>
+      <div className="optionText">Github: <a target="_blank" href="https://github.com/z0ccc/Reddit-Wherever" rel="noreferrer">https://github.com/z0ccc/Reddit-Wherever</a></div>
+    </div>
+  );
 };
 
 export default OptionsPage;
--- src/options.tsx
+++ src/options.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 import OptionsPage from './OptionsPage';
-import './popup.css';
+import './options.css';
 
 const mountNode = document.getElementById('options');
 ReactDOM.render(<OptionsPage />, mountNode);
