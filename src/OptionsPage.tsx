7e1984830b626e961bc1f9bd2ecf313c256a6ecd
z0ccc <z0cccccccc@gmail.com>
2021-08-11 15:17:40 -0400

    Added click only option

--- src/OptionsPage.tsx
+++ src/OptionsPage.tsx
@@ -6,6 +6,20 @@ import { useState, useEffect } from 'react';
 
 import './App.css';
 
+const setClickOnly = () => {
+  chrome.storage.sync.get('clickOnly', ({ clickOnly }) => {
+    const value = !clickOnly;
+    chrome.storage.sync.set({ clickOnly: value });
+    if (value === true) {
+      chrome.action.setIcon({
+        path: {
+          16: '../images/reddit_16.png',
+        },
+      });
+    }
+  });
+};
+
 const OptionsPage = () => {
   return (
     <div>
@@ -18,14 +32,30 @@ const OptionsPage = () => {
         <label htmlFor="theme">Extension theme</label>
       </div>
       <div className="optionItemWrap">
-        <input type="checkbox" id="clickOnly" name="clickOnly" />
-        <label htmlFor="clickOnly">Run browser popup only when icon is clicked</label>
+        <input
+          type="checkbox"
+          id="clickOnly"
+          name="clickOnly"
+          onChange={setClickOnly}
+        />
+        <label htmlFor="clickOnly">
+          Run browser popup only when icon is clicked
+        </label>
       </div>
       <div className="optionItemWrap">
         <input type="checkbox" id="ytDefault" name="ytDefault" />
         <label htmlFor="ytDefault">Show YouTube comments as default</label>
       </div>
-      <div className="optionText">Github: <a target="_blank" href="https://github.com/z0ccc/Reddit-Wherever" rel="noreferrer">https://github.com/z0ccc/Reddit-Wherever</a></div>
+      <div className="optionText">
+        Github:{' '}
+        <a
+          target="_blank"
+          href="https://github.com/z0ccc/Reddit-Wherever"
+          rel="noreferrer"
+        >
+          https://github.com/z0ccc/Reddit-Wherever
+        </a>
+      </div>
     </div>
   );
 };
