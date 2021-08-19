bc4ecccf3ef3fd75cf642b4eabf3f81e024b083d
z0ccc <z0cccccccc@gmail.com>
2021-08-19 16:47:48 -0400

    Added hide comment button

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -13,6 +13,7 @@ import {
 const Comment = ({ comment, permalink }: any) => {
   const [replies, setReplies] = useState<CommentType[][]>([]);
   const [loading, setLoading] = useState<boolean>();
+  const [hidden, setHidden] = useState<boolean>();
 
   const loadMore = async () => {
     setLoading(true);
@@ -23,6 +24,11 @@ const Comment = ({ comment, permalink }: any) => {
     await Promise.all(promisesFetch).then((value: any) => {
       setReplies(value);
     });
+  };
+
+  const hideComment = () => {
+    console.log('hello');
+    setHidden(true);
   };
 
   return (
@@ -56,35 +62,42 @@ const Comment = ({ comment, permalink }: any) => {
           )}
         </>
       ) : (
-        <div className="comment">
-          <div className="commentInfo">
-            <div className="infoWrap">
-              <a
-                href={`https://reddit.com/u/${comment.data.author}`}
-                target="_blank"
-                className="commentTitle"
-                rel="noreferrer"
-              >
-                {comment.data.author}
-              </a>
-              <div className="info">
-                {formatNumber(comment.data.score)} points
+        <>
+          {hidden ? (
+            <div>j</div>
+          ) : (
+            <div className="comment">
+              <div className="commentInfo">
+                <div className="infoWrap">
+                  <a
+                    href={`https://reddit.com/u/${comment.data.author}`}
+                    target="_blank"
+                    className="commentTitle"
+                    rel="noreferrer"
+                  >
+                    {comment.data.author}
+                  </a>
+                  <div className="info">
+                    {formatNumber(comment.data.score)} points
+                  </div>
+                  <div className="info">
+                    {convertDate(comment.data.created_utc)}
+                  </div>
+                  <button className="info" type="submit" onClick={hideComment}>[-]</button>
+                </div>
+                <div className="commentBody">
+                  {Parser(decodeHtml(comment.data.body_html))}
+                </div>
               </div>
-              <div className="info">
-                {convertDate(comment.data.created_utc)}
-              </div>
-            </div>
-            <div className="commentBody">
-              {Parser(decodeHtml(comment.data.body_html))}
-            </div>
-          </div>
-          <div className="child">
-            {comment.data.replies &&
+              <div className="child">
+                {comment.data.replies &&
               comment.data.replies.data.children.map((object: any) => (
                 <Comment comment={object} permalink={permalink} />
               ))}
-          </div>
-        </div>
+              </div>
+            </div>
+          )}
+        </>
       )}
     </>
   );
