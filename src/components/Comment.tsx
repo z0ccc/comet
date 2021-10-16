b40646d8bcab39e29e9931b303fd2c7539c2af5b
z0ccc <z0cccccccc@gmail.com>
2021-10-16 16:02:27 -0400

    Updated version number

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -8,17 +8,14 @@ interface ComponentProps {
   permalink: string;
 }
 
-const Comment = ({ comment, permalink }: ComponentProps) => {
-  console.log(comment);
-  return (
-    <>
-      {comment.kind === 'more' ? (
-        <LoadMore comment={comment} permalink={permalink} />
-      ) : (
-        <CommentInfo comment={comment} permalink={permalink} />
-      )}
-    </>
-  );
-};
+const Comment = ({ comment, permalink }: ComponentProps) => (
+  <>
+    {comment.kind === 'more' ? (
+      <LoadMore comment={comment} permalink={permalink} />
+    ) : (
+      <CommentInfo comment={comment} permalink={permalink} />
+    )}
+  </>
+);
 
 export default Comment;
