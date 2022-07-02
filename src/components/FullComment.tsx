47877ea91245f0314f201ecc512203ca3ce2922a
z0ccc <z0ccc@protonmail.com>
2022-07-01 23:55:35 -0400

    update comment score when voted

--- src/components/FullComment.tsx
+++ src/components/FullComment.tsx
@@ -1,10 +1,9 @@
 import * as React from 'react';
-import { Dispatch, SetStateAction } from 'react';
+import { Dispatch, SetStateAction, useState, useEffect } from 'react';
 import Parser from 'html-react-parser';
 import { DataType } from './types';
-import { convertDate, fixHref, formatNumber } from './main';
+import { convertDate, fixHref, formatNumber, getVote, getDir } from './main';
 import Comment from './Comment';
-import CommentScore from './CommentScore';
 
 interface ComponentProps {
   comment: DataType;
@@ -12,42 +11,70 @@ interface ComponentProps {
   setCollapse: Dispatch<SetStateAction<boolean>>;
 }
 
-const FullComment = ({ comment, permalink, setCollapse }: ComponentProps) => (
-  <>
-    <div className="commentInfo">
-      <CommentScore comment={comment} />
-      <div>
-        <div className="infoWrap">
+const FullComment = ({ comment, permalink, setCollapse }: ComponentProps) => {
+  const [vote, setVote] = useState<number>(0);
+  const [score, setScore] = useState<number>(comment.data.score);
+
+  useEffect(() => {
+    setVote(getVote(comment.data.likes));
+  }, []);
+
+  const handleVote = (voteDir: number) => {
+    const dir: number = getDir(voteDir, vote);
+    chrome.runtime.sendMessage({ id: comment.data.name, dir });
+    setVote(dir);
+    setScore(score + (dir === 0 ? voteDir * -1 : dir));
+  };
+  return (
+    <>
+      <div className="commentInfo">
+        <div className="postScore">
           <button
-            className="info"
+            className={`arrow arrowMargin ${vote === 1 ? 'upmod' : 'upvote'}`}
             type="button"
-            onClick={() => setCollapse(true)}
-          >
-            [–]
-          </button>
-          <a
-            href={`https://reddit.com/u/${comment.data.author}`}
-            target="_blank"
-            className="commentTitle"
-            rel="noreferrer"
-          >
-            {comment.data.author}
-          </a>
-          <div className="info">{formatNumber(comment.data.score)} points</div>
-          <div className="info">{convertDate(comment.data.created_utc)}</div>
+            aria-label="Upvote"
+            onClick={() => handleVote(1)}
+          />
+          <button
+            className={`arrow arrowMargin ${vote === -1 ? 'downmod' : 'downvote'}`}
+            type="button"
+            aria-label="Downvote"
+            onClick={() => handleVote(-1)}
+          />
         </div>
-        <div className="commentBody">
-          {Parser(fixHref(comment.data.body_html))}
+        <div>
+          <div className="infoWrap">
+            <button
+              className="info"
+              type="button"
+              onClick={() => setCollapse(true)}
+            >
+              [–]
+            </button>
+            <a
+              href={`https://reddit.com/u/${comment.data.author}`}
+              target="_blank"
+              className="commentTitle"
+              rel="noreferrer"
+            >
+              {comment.data.author}
+            </a>
+            <div className="info">{formatNumber(score)} points</div>
+            <div className="info">{convertDate(comment.data.created_utc)}</div>
+          </div>
+          <div className="commentBody">
+            {Parser(fixHref(comment.data.body_html))}
+          </div>
         </div>
       </div>
-    </div>
-    <div className="child">
-      {comment.data.replies &&
+      <div className="child">
+        {comment.data.replies &&
         comment.data.replies.data.children.map((object: DataType) => (
           <Comment comment={object} permalink={permalink} />
         ))}
-    </div>
-  </>
-);
+      </div>
+    </>
+  );
+};
 
 export default FullComment;
